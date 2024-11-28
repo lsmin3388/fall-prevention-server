5291efcdf9448bd5dd33c3eb447e9221013beca2
wodydl0 <poty9798@naver.com>
2024-11-28 18:48:03 +0900

    feat: region update api 추가

--- fall-prevention-api/src/main/java/com/happyaging/fallprevention/region/api/RegionController.java
+++ fall-prevention-api/src/main/java/com/happyaging/fallprevention/region/api/RegionController.java
@@ -1,2 +1,38 @@
-package com.happyaging.fallprevention.region.api;public class RegionController {
+package com.happyaging.fallprevention.region.api;
+
+import com.happyaging.fallprevention.region.dto.RegionRequestDto;
+import com.happyaging.fallprevention.region.entity.Region;
+import com.happyaging.fallprevention.region.service.RegionService;
+import com.happyaging.fallprevention.util.api.ApiResponse;
+import com.happyaging.fallprevention.util.api.ApiSuccessResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/region")
+public class RegionController {
+    private final RegionService regionService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiSuccessResult<Region>> createRegion(
+            @Valid @RequestBody RegionRequestDto requestDto
+            ) {
+        regionService.createRegion(requestDto.toEntity());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(HttpStatus.CREATED));
+    }
+
+    @PutMapping("/update/{regionId}")
+    public ResponseEntity<ApiSuccessResult<Object>> updateRegion(
+            @PathVariable Long regionId,
+            @Valid @RequestBody RegionRequestDto requestDto
+    ) {
+        regionService.updateRegion(regionId, requestDto.regionName());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK));
+    }
 }
--- fall-prevention-core/src/main/java/com/happyaging/fallprevention/region/service/RegionService.java
+++ fall-prevention-core/src/main/java/com/happyaging/fallprevention/region/service/RegionService.java
@@ -1,5 +1,6 @@
 package com.happyaging.fallprevention.region.service;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import com.happyaging.fallprevention.region.entity.Region;
@@ -7,6 +8,9 @@ import com.happyaging.fallprevention.region.exception.RegionNotFoundException;
 import com.happyaging.fallprevention.region.persistence.RegionRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,27 @@ public class RegionService {
 	public Region getRegionByRegionName(String regionName) {
 		return regionRepository.findByRegionName(regionName)
 			.orElseThrow(RegionNotFoundException::new);
+	}
+
+	@Transactional
+	public void createRegion(@Valid Region region) {
+		regionRepository.save(region);
+	}
+
+	@Transactional
+	public void updateRegion(Long regionId, String regionName) {
+		Region region = regionRepository.findById(regionId)
+				.orElseThrow(RegionNotFoundException::new);
+		region.updateRegionName(regionName);
+	}
+
+
+	@Transactional
+	public void deleteRegion(@Valid Region region) {
+		regionRepository.delete(region);
+	}
+
+	public List<Region> getAllRegions() {
+		return regionRepository.findAll();
 	}
 }
